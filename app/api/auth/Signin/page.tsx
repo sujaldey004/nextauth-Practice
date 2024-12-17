39feeec3c2a78994fe8c62bf365745ccb9c7deb2
sujaldey004 <sujaldey2920@gmail.com>
2024-12-17 20:40:44 +0530

    credentials_working

---
+++ app/api/auth/Signin/page.tsx
@@ -0,0 +1,10 @@
+
+export default function SignIn(){
+  return <>
+    <div>
+      <input type="email" placeholder="john@gmail.com"/>
+      <input type="password" placeholder="123!@#"/>
+    </div>
+    <button>Submit</button>
+  </>
+}
--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,13 +1,32 @@
-import Email from "next-auth/providers/email";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const NEXT_AUTH = {
-    providers:{
-        CredentialsProviders:{
+    providers:[
+        CredentialsProvider({
             name:'Email',
             credentials:{
                 email: {label: "email", type: 'text', placeholder:'sam2004@gmail.com'},
                 password: {label: 'password', type: 'password', placeholder: 'asd123!@#'}
+            },
+            async authorize(credentials : any){
+              console.log(credentials)
+              return {
+                email: credentials.email
+              }
             }
-        }
+        })
+    ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks:{
+    session:(session, token, user)=>{
+      console.log(session);
+      if(session && session.user){
+        session.id = token.sub 
+      }
+      return session
     }
+  },
+  pages:{
+    signin: "/signin"
+  }
 }
